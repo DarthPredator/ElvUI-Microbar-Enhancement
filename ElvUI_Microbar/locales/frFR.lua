cde3d930c889436d19c4ce56532ae9de95c2e8f8
Darthpred <darthpred@gmail.com>
2012-04-10 23:05:37 +0400

    Slight change for French locale

--- ElvUI_Microbar/locales/frFR.lua
+++ ElvUI_Microbar/locales/frFR.lua
@@ -2,10 +2,10 @@
 local L = AceLocale:NewLocale("ElvUI", "frFR");
 if not L then return; end
 
-L["2 rows"] = "6 boutons par ligne, 2 lignes au total"
-L["3 rows"] = "4 boutons par ligne, 3 lignes au total"
-L["4 rows"] = "3 boutons par ligne, 4 lignes au total"
-L["6 rows"] = "2 boutons par ligne, 6 lignes au total"
+L["2 rows"] = "2 lignes"
+L["3 rows"] = "3 lignes"
+L["4 rows"] = "4 lignes"
+L["6 rows"] = "6 lignes"
 L["Change the positioning of buttons on Microbar."] = "Modifie le positionnement des boutons de la microbar"
 L["Hide in Combat"] = "Masquer en combat"
 L["Hide Microbar in combat."] = "Masquer la microbar en combat"
