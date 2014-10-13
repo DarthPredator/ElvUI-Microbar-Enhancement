6243fb07f5553fc1abfd629a06b942ed402db89b
Darthpred <darthpred@gmail.com>
2014-10-14 02:22:57 +0400

    A little tweak

--- ElvUI_Microbar/locales/english.lua
+++ ElvUI_Microbar/locales/english.lua
@@ -1,4 +1,4 @@
-﻿-- English localization file forenGB.
+﻿-- English localization file for enGB.
 local AceLocale = LibStub:GetLibrary("AceLocale-3.0");
 local L = AceLocale:NewLocale("ElvUI", "enUS", true);
 if not L then return; end
@@ -7,4 +7,4 @@ L["Set Scale"] = "Set Scale"
 L["Sets Scale of the Micro Bar"] = "Sets Scale of the Micro Bar"
 L["As Letters"] = true
 L["Replace icons with just letters.\n|cffFF0000Warning:|r this will disable original Blizzard's tooltips for microbar."] = true
-L["Use 12th button for accessing in game shop, if disabled will bring up the support panel.\n|cffFF0000Warning:|r this option requieres to reload the ui to take effect."] = true
+L["Use 10th button for accessing in game shop, if disabled will bring up the support panel.\n|cffFF0000Warning:|r this option requieres to reload the ui to take effect."] = true
